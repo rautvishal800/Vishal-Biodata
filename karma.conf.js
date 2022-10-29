5c1e57b8e49bf96ad2762481da23d5a7755ce874
rautvishal <rautvishal800@gmail.com>
2022-10-29 17:31:10 +0530

    Deleted Package Lock + Other Changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/vishal-bio-data'),
+      dir: require('path').join(__dirname, './coverage/vishal-biodata'),
       subdir: '.',
       reporters: [
         { type: 'html' },
